1374273008324778a4bf5d7ca30f034172ff262a
Jeslin Samuel <jeslin.samuel@temple.edu>
2019-09-11 18:38:01 -0400

    Added button functionality that changes the textbox text to "You have clicked the button" upon button press.

--- app/src/main/java/edu/temple/helloworldv2/MainActivity.java
+++ app/src/main/java/edu/temple/helloworldv2/MainActivity.java
@@ -3,12 +3,24 @@ package edu.temple.helloworldv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                TextView text = findViewById(R.id.textView);
+                text.setText("You have clicked the button");
+            }
+        });
     }
 }
